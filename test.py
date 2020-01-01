5f277c140864a346034962efbf6a93d8138b1f2b
wz5mz <wz5mz@virginia.edu>
2019-12-31 16:52:07 -0800

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,7 @@
 import shlex
 import subprocess
 
-def send_intent(wx_string, wx_daily, wx_icon, wx_city, wx_state):
-    args = shlex.split("/data/data/com.termux/files/usr/bin/am broadcast "
-                       "--user 0 "
-                       "-a net.dinglish.tasker.wx ")
-    subprocess.Popen(args)
+args = shlex.split("/data/data/com.termux/files/usr/bin/am broadcast "
+                   "--user 0 "
+                   "-a net.dinglish.tasker.wx ")
+subprocess.Popen(args)
