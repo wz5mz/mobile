1de5a20239f7b30c6cf1ad7ee886091b06ffe249
wz5mz <wz5mz@virginia.edu>
2019-12-31 17:17:20 -0800

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import subprocess
 def send_var(testvar):
     args = shlex.split("/data/data/com.termux/files/usr/bin/am broadcast "
                        "--user 0 "
-                       "-a net.dinglish.tasker.schwab_report "
+                       "-a net.dinglish.tasker.schwabreport "
                        "-e testvar '{}' ".format(testvar.replace("''","")))
     print(args)
 #    subprocess.Popen(args)
