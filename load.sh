123498e4e5a65f9f81378fc60a20fcbd4c36f2ab
wz5mz <wz5mz@virginia.edu>
2019-12-31 16:01:11 -0800

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,1 +1,11 @@
+#! /data/data/com.termux/files/usr/bin/bash
 
+cd ~/.termux/tasker/
+git clone "https://github.com/wz5mz/mobile.git"
+for file in ~/.termux/tasker/mobile/*
+do
+   termux-fix-shebang "$file"
+   chmod +x "$file"
+done
+mv -v ~/.termux/tasker/mobile/* ~/.termux/tasker/
+rm -rf mobile
