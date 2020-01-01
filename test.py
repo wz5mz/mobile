f446e3defe515a9ca71006aeb46748ff84c9a62f
wz5mz <wz5mz@virginia.edu>
2019-12-31 17:25:02 -0800

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def send_var(testvar):
     args = shlex.split("/data/data/com.termux/files/usr/bin/am broadcast "
                        "--user 0 "
                        "-a net.dinglish.tasker.schwabreport "
-                       "-e testvar '{}' ".format(testvar.replace("''","")))
+                       "-e testvar '{}' ".format(testvar.replace("'","")))
     subprocess.Popen(args)
 
-send_var('$92,000.00')
+send_var('$92000.00')
